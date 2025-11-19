42f13be70373277919f3d273ead1ff02db11cd7f
IznaIn <mohammadiznain@gmail.com>
2025-11-19 15:46:14 +0530

    removed comments

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -2,6 +2,7 @@ function Footer() {
   return (
     <footer className="text-center py-3 bg-dark mt-4">
       <p className="text-white">© 2025 RealState. All rights reserved.</p>
+      <h6 className="text-white">Developed by MohammadIznain</h6>
     </footer>
   );
 }
